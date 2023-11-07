ecfbba660ba8ce19c287416eccfa1d79aab9acbd
ruo3531 <ruozhenwork@gmail.com>
2023-11-07 15:17:34 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+document.getElementById("id-name");
+document.getElementByClassName("class-name");
+document.getElementByTagName("tag-name");
+
+var test = document.getElementById("id-name")
+// 用 test 去接物件
+
+test.innerText = "I've been changed"
+// 最常使用,獲取或設置元素內的文字
+test.innerHTML
+// 獲取或設置元素內包含的 HTML 標籤
+
+
